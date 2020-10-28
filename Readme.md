9d68a4ee45a55f683104bba5613f2d2caab7c861
aldiprabowo <aldi.prabowo@students.amikom.ac.id>
2020-10-28 11:02:10 +0700

    Add project files.

---
+++ Readme.md
@@ -0,0 +1,14 @@
+# The Next Car
+- Aplikasi ini adalah program sederhana yang menerapkan konsep MVC (Model View Controller)
+- mesin dapat dihidupkan ketika pintu sudah tertutup & terkunci serta aki sudah menyala
+
+## Fungsionalitas
+-  User dapat menekan tombol yang tersedia dan mengikuti perintahnya
+-  User dapat memulai program ketika kondisi yang disebutkan telah terpenuhi
+-  gambar gps dan kecepatan hanya aksesoris (pemanis)
+
+## Cara Kerja
+- Model terletak pada folder Model yang berisikan class `Door.cs` dan `AccuBattery.cs`
+- View terletak di `MainWindow.xaml`
+- Controller terletak pada folder Controller yang berisi class `AccuBatteryController.cs`,`DoorController.cs`,dan `Car.cs`
+- Function terdapat pada class `MainWindow.xaml.cs`
---
+++ TheNextCar/Controller/Car.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheNextCar.Controller
+{
+    class Car
+    {
+        AccubatteryController accubatteryController;
+        DoorController doorController;
+        OnCarEngineStatusChanged callbackCarEngineStatusChanged;
+
+        public Car(AccubatteryController accubatteryController, DoorController doorController, OnCarEngineStatusChanged callbackCarEngineStatusChanged)
+        {
+            this.accubatteryController = accubatteryController;
+            this.doorController = doorController;
+            this.callbackCarEngineStatusChanged = callbackCarEngineStatusChanged;
+        }
+
+        public void turnOnPower()
+        {
+            this.accubatteryController.turnOn();
+        }
+
+        public void turnOffPower()
+        {
+            this.accubatteryController.turnOff();
+        }
+
+        public bool powerIsReady()
+        {
+            return this.accubatteryController.accubatteryIsOn();
+        }
+
+        public void closeTheDoor()
+        {
+            this.doorController.close();
+        }
+        public void openTheDoor()
+        {
+            this.doorController.open();
+        }
+
+        public void lockTheDoor()
+        {
+            this.doorController.activateLock();
+        }
+        public void unlockTheDoor()
+        {
+            this.doorController.unlock();
+        }
+
+        private bool doorIsClosed()
+        {
+            return this.doorController.isClosed();
+        }
+        private bool doorIsLocked()
+        {
+            return this.doorController.isLocked();
+        }
+
+        public void toggleStartEngineButton()
+        {
+            if (!doorIsClosed())
+            {
+                this.callbackCarEngineStatusChanged.carEngineStatusChanged("STOPPED", "door is open");
+                return;
+            }
+            if (!doorIsLocked())
+            {
+                this.callbackCarEngineStatusChanged.carEngineStatusChanged("STOPPED", "door unlocked");
+                return;
+            }
+            if (!powerIsReady())
+            {
+                this.callbackCarEngineStatusChanged.carEngineStatusChanged("STOPPED", "no power available");
+                return;
+            }
+            this.callbackCarEngineStatusChanged.carEngineStatusChanged("STARTED", "Engine started");
+        }
+
+        public void toggleTheLockButton()
+        {
+            if (!doorIsLocked())
+            {
+                this.lockTheDoor();
+            }
+            else
+            {
+                this.unlockTheDoor();
+            }
+        }
+
+        public void toggleTheDoorButton()
+        {
+            if (!doorIsClosed())
+            {
+                this.closeTheDoor();
+            }
+            else
+            {
+                this.openTheDoor();
+            }
+        }
+
+        public void toggleThePowerButton()
+        {
+            if (!powerIsReady())
+            {
+                this.turnOnPower();
+            }
+            else
+            {
+                this.turnOffPower();
+            }
+        }
+    }
+
+    interface OnCarEngineStatusChanged
+    {
+        void carEngineStatusChanged(string value, string message);
+    }
+}
